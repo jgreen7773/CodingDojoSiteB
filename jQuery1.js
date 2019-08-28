97ce3033fa5d871d666564186cfc9a9b0c65796f
Jamie Green (Song Kran) <James@DeathRiddle-iMac.local>
2019-08-28 15:07:56 -0700

    Updated some jQuery, not finished yet.

---
+++ jQuery1.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    // alert("working")
+    $('.navtext').hover(function(){
+        $(this).fadeOut(100);$(this).fadeIn(6000);});
+    $('.logo').click(function(){
+    
+    $('.logo').css('margin', '50px 0px 0px 50px')});
+
+    $('.side_menu_list_wrapper').click(function(){
+        $('navlinks2').hide();
+    }
+
+
+
+
+
+})
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // $('.getting_started h3').on(function(){text("We're getting started")});
