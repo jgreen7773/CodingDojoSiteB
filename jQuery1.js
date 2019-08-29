115ce0aa661b922a9b7334ab3906b36e349fd288
Jamie Green (Song Kran) <James@DeathRiddle-iMac.local>
2019-08-28 21:17:59 -0700

    I completed the jQuery Functions assignment and I LOVE IT. Feeling like I'm getting it.

--- jQuery1.js
+++ jQuery1.js
@@ -7,9 +7,47 @@ $(document).ready(function(){
     $('.logo').css('margin', '50px 0px 0px 50px')});
 
     $('.side_menu_list_wrapper').click(function(){
-        $('navlinks2').hide();
-    }
+        $('navlinks2').hide()
+    })
 
+    $('button').click(function(){
+        $('.below').hide()
+    })
+    
+    $('button').click(function(){
+        $('.side_menu_list_wrapper a').show()
+    })
+
+    $('.navlinks2').addClass('bg-primary');
+
+    $('.getting_started').toggle();
+
+    // $('.text_size_and_box p large').on("click", function(){
+    //     $('body').addclass('.textbodybig');
+    // })
+
+    // $('.text_size_and_box p normal').on("click", function(){
+    //     $('body').addclass('.textbodynormal');
+    // })
+
+    $('.applications').click(function(){
+        $('.applications p').slideToggle(4000)
+    })
+
+    $('.text_size_and_box p').before(function(){
+        $('.text_size_and_box p').text('hello!')
+    })
+
+    $('.navbox li a').html(function(){
+        $('.navbox li a').text('Whats up')
+    })
+
+    $('input').keyup(function(){
+        var value = $(this).val();
+        $("p").text(value);
+        alert(value);
+    })
+    .keyup();
 
 
 
